efbecf2590fa6408405a99a96bfe252a5bd8237b
forestsource <itumonohito@gmail.com>
2016-07-16 19:27:04 +0900

    first intent

--- app/src/main/java/forestsource/simplemail/MultiActivity.java
+++ app/src/main/java/forestsource/simplemail/MultiActivity.java
@@ -1,13 +1,24 @@
 package forestsource.simplemail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 
 public class MultiActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        Button btnNext = (Button) this.findViewById(R.id.button1);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MultiActivity.this , SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
